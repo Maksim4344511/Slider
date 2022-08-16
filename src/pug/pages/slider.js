91192dcee1446700c1509cb445ac5613452ce776
Maksim <maxim4344511@gmail.com>
2022-08-16 15:37:21 +0300

    The first slider was created in the zgp, I'm redoing it through Class

--- src/pug/pages/slider.js
+++ src/pug/pages/slider.js
@@ -1,13 +1,35 @@
 "use strict";
 
-let firstPolzunok = document.querySelector('.slider-polzunok1');
-let dial = document.querySelector('.slider-dial');
+class Slider {
+  constructor(name){
+    this.name = name;
+  }
 
-firstPolzunok.onmousedown = function(event) {
+  createPolzunokHTML(){
+
+    let wrap = document.body.firstChild;
+
+    let dial = document.createElement('div');
+    dial.className = 'slider-dial';
+    wrap.append(dial);
+
+    let firstPolzunok = document.createElement('div');
+    firstPolzunok.className = 'slider-polzunok1';
+    dial.append(firstPolzunok); 
+   
+  }
+
+  moveFirstPolzunok(){
+
+    //let useX = event.clientX - firstPolzunok.getBoundingClientRect().left;
+    console.log(document.querySelector('.slider-dial'));
+    console.log('sdf');
+    /*
+  firstPolzunok.onmousedown = function(event) {
     event.preventDefault(); // предотвратить запуск выделения (действие браузера)????? первый
 
 
-    let useX = event.clientX  - firstPolzunok.getBoundingClientRect().left;//я бы оставил event.clientX, но ползунок дергается
+    
 
     document.addEventListener('mousemove', onMouseMove); // двигаем мышку
     document.addEventListener('mouseup', onMouseUp); //отпустил мышку
@@ -36,4 +58,15 @@ firstPolzunok.onmousedown = function(event) {
 
 firstPolzunok.ondragstart = function() {
     return false;    
-};///  и опять предотвращаем выделение документа. Пока оставляю оба.
+};///  и опять предотвращаем выделение документа. Пока оставляю оба.*/
+  }
+
+
+};
+
+
+let test = new Slider();
+
+test.createPolzunokHTML();
+
+test.moveFirstPolzunok();
