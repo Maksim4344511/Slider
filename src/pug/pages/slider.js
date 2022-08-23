a4c8799311a1884690945a1772694c8391699129
Maksim <maxim4344511@gmail.com>
2022-08-23 19:10:43 +0300

    A second slider was made, a field of values was added, and the names of variables were put in order.

--- src/pug/pages/slider.js
+++ src/pug/pages/slider.js
@@ -7,59 +7,113 @@ class Slider {
   constructor(elem){
     
     this.wrap = elem;
+
+    this.title = document.createElement('div');
+    this.title.className = 'slider-title';
+    this.title.innerHTML = 'Range slider';
+    this.wrap.append(this.title);
+
     this.dial = document.createElement('div');    
     this.wrap.append(this.dial);
-    this.dial.className = 'slider-dial';
+    this.dial.className = 'slider-dial'; 
+
     this.firstPolzunok = document.createElement('div');
     this.firstPolzunok.className = 'slider-polzunok1';
-    this.dial.append(this.firstPolzunok);  
+    this.dial.append(this.firstPolzunok);
+    this.firstPolzunok.style.left = '0';
     
-    this.onpointermove = this.onpointermove.bind(this);
-    this.pointerup = this.pointerup.bind(this);
-   
+    this.secondPolzunok = document.createElement('div');
+    this.secondPolzunok.className = 'slider-polzunok2';
+    this.dial.append(this.secondPolzunok); 
+    this.secondPolzunok.style.left = '110px';
+    
+    this.firstValue = document.createElement('div');
+    this.firstValue.className = 'slider-value1';
+    this.firstValue.innerHTML = this.firstPolzunok.style.left.slice(0, -2);
+    this.title.append(this.firstValue);
+    
+    this.secondValue = document.createElement('div');
+    this.secondValue.className = 'slider-value2';
+    this.secondValue.innerHTML = this.secondPolzunok.style.left.slice(0, -2);
+    this.title.append(this.secondValue);      
+    
+    this.onPointerMove = this.onPointerMove.bind(this);
+    this.onPointerUp = this.onPointerUp.bind(this);   
         
   }
 
-  init(){       
-    this.firstPolzunok.addEventListener('pointerdown', this.onpointerdown.bind(this), false);         
+  init(){   
+
+    this.firstPolzunok.addEventListener('pointerdown', this.onPointerDown.bind(this), false);
+    this.secondPolzunok.addEventListener('pointerdown', this.onPointerDown.bind(this), false);    
+        
   }
 
-  onpointerdown(event){   
-    document.addEventListener('pointermove',  this.move, false); 
-    document.addEventListener('pointerup', this.pointerup, false); 
+  onPointerDown(event){   
+
+    document.addEventListener('pointermove',  this.onPointerMove, false); 
+    document.addEventListener('pointerup', this.onPointerUp, false); 
     
-    let x = document.querySelector('.slider-polzunok1');
+    let firstPolzunok = document.querySelector('.slider-polzunok1');
+    let secondPolzunok = document.querySelector('.slider-polzunok2');
 
-    let shiftX = event.clientX - x.getBoundingClientRect().left;
-    x.classList.add('shift');
-    x.shift = shiftX;
+    if (event.currentTarget == firstPolzunok){  
+      this.a = 5;    
+
+      firstPolzunok.shiftX = event.clientX - firstPolzunok.getBoundingClientRect().left; 
      
+    } else if (event.currentTarget == secondPolzunok){
+      this.a = 6;
+
+      secondPolzunok.shiftX = event.clientX - secondPolzunok.getBoundingClientRect().left;     
+    }       
   }
-
  
-  onpointermove(event){    
+  onPointerMove(event){   
 
-    let x = document.querySelector('.slider-polzunok1');
-    let y = document.querySelector('.slider-dial');    
+    let dial = document.querySelector('.slider-dial');
+    let firstPolzunok = document.querySelector('.slider-polzunok1');
+    let secondPolzunok = document.querySelector('.slider-polzunok2');  
+    
+    
+    if (this.a == 5){  
 
-    let newLeft = event.clientX - x.shift - y.getBoundingClientRect().left;
+      let newLeftFirstP = event.clientX - firstPolzunok.shiftX - dial.getBoundingClientRect().left;
 
-    if (newLeft < 0) {
-      newLeft = 0;
-    }
+      if (newLeftFirstP < 0) {
+        newLeftFirstP = 0;
+      };
 
-    let rightEdge = y.offsetWidth - x.offsetWidth;
+      if (newLeftFirstP > +secondPolzunok.style.left.slice(0, -2)){
+        newLeftFirstP = +secondPolzunok.style.left.slice(0, -2);
+      };
 
-    if (newLeft > rightEdge) {
-      newLeft = rightEdge;
-    }
+      firstPolzunok.style.left = newLeftFirstP + 'px';
+      this.firstValue.innerHTML = Math.round(newLeftFirstP);
 
-    x.style.left = newLeft + 'px';      
+    } else if (this.a == 6){
+
+      let newLeftSecondP = event.clientX - secondPolzunok.shiftX - dial.getBoundingClientRect().left;   
+
+      let rightEdge = dial.offsetWidth - secondPolzunok.offsetWidth;
+
+      if (newLeftSecondP > rightEdge) {
+        newLeftSecondP = rightEdge;
+      }; 
+      
+      if (newLeftSecondP < +firstPolzunok.style.left.slice(0, -2)){
+        newLeftSecondP = +firstPolzunok.style.left.slice(0, -2);
+      };
+
+      secondPolzunok.style.left = newLeftSecondP + 'px';
+      this.secondValue.innerHTML = Math.round(newLeftSecondP);
+    };        
   }
   
-  pointerup(){     
-    document.removeEventListener('pointerup', this.pointerup); 
-    document.removeEventListener('pointermove', this.onpointermove);    
+  onPointerUp(){   
+
+    document.removeEventListener('pointerup', this.onPointerUp); 
+    document.removeEventListener('pointermove', this.onPointerMove);    
 
   }
 }
@@ -68,8 +122,9 @@ class Slider {
 
 const slider = new Slider(document.querySelector('.slider-wrap'));
 
+
 slider.init();
-//console.log(slider);
+
 
 
 
